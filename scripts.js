765a7da89fc9d0fd067102ebbf8c204c3b5922e7
NicoBoni55 <bonifrank55@gmail.com>
2024-07-31 15:13:34 -0400

    .

--- scripts.js
+++ scripts.js
@@ -4,5 +4,8 @@
 */
 
 document.addEventListener('DOMContentLoaded', () => {
-  /* DO SOMETHING */
+  fetch("http://127.0.0.1:5500/login", {
+    method: "POST",
+    headers: "application/json"
+  })
 });
